4293472bbbe7237a0192cb43673192df97373794
tedkkraft <tedkkraft@gmail.com>
2020-08-26 16:45:28 -0400

    DB setup

---
+++ db/seeds.sql
@@ -0,0 +1,5 @@
+USE burgers_db;
+
+INSERT INTO burgers (burger_name) VALUES ('Quarter Pounder');
+INSERT INTO burgers (burger_name) VALUES ('Big Mac');
+INSERT INTO burgers (burger_name) VALUES ('Jr. Bacon Cheeseburger');
